a507204fb4cad96cfa3a78a65240022cd715ca4e
eolagbaju <eoolagbaju@gmail.com>
2021-04-19 16:34:33 +0000

    Add and change tests for get_coverage

--- tests/testthat/test-get_coverage.R
+++ tests/testthat/test-get_coverage.R
@@ -1,13 +1,85 @@
+gtex_metadata <- recount::all_metadata("gtex")
+
+gtex_metadata <- gtex_metadata %>%
+    as.data.frame() %>%
+    dplyr::filter(project == "SRP012682")
+
+# obtain path to example bw on recount2
+url <- recount::download_study(
+    project = "SRP012682",
+    type = "samples",
+    download = FALSE
+)
+
+bw_path <- .file_cache(url[1])
+
 test_that("get_chr_info works", {
-    all_UCSC_chr <- GenomeInfoDb::getChromInfoFromUCSC("hg38")[["chrom"]]
     chrs <- c("chr1", "chr2", "chr3")
     test_info <- get_chr_info(chrs = chrs, genome = "hg38")
-    expect_true(all_UCSC_chr %contain% "chr1")
-    expect_true(all_UCSC_chr %contain% chrs)
+
+    expect_false(test_info %contain% "chr4")
     expect_error(get_chr_info(chrs = "chromononsense", genome = "hg38"), "Non-UCSC chromosome format was entered")
     expect_equal(nrow(test_info), length(chrs))
-    expect_type(test_info[[1]], "character")
+    expect_type(test_info[["chrom"]], "character")
     expect_type(test_info[[2]], "integer")
     expect_type(test_info[[3]], "logical")
     expect_type(test_info[[4]], "logical")
+})
+
+test_that("get_coverage works", {
+    test_chrs <- c("chr21", "chr22")
+    test_coverage <- get_coverage(
+        bw_paths = bw_path,
+        auc_raw = gtex_metadata[["auc"]][1],
+        auc_target = 40e6 * 100,
+        chrs = test_chrs
+    )
+
+    expect_error(
+        get_coverage(
+            bw_paths = bw_path,
+            auc_raw = "Not a number",
+            auc_target = 40e6 * 100,
+            chrs = c("chr21", "chr22")
+        ),
+        "Please enter a valid number for the auc values"
+    )
+    expect_error(
+        get_coverage(
+            bw_paths = bw_path,
+            auc_raw = gtex_metadata[["auc"]][1],
+            auc_target = "Not a number",
+            chrs = c("chr21", "chr22")
+        ),
+        "Please enter a valid number for the auc values"
+    )
+    expect_error(
+        get_coverage(
+            bw_paths = "",
+            auc_raw = gtex_metadata[["auc"]][1],
+            auc_target = 40e6 * 100,
+            chrs = c("chr21", "chr22")
+        ),
+        "Please check your bigwig file paths are correct"
+    )
+    expect_error(
+        get_coverage(
+            auc_raw = gtex_metadata[["auc"]][1],
+            auc_target = 40e6 * 100,
+            chrs = c("chr21", "chr22")
+        ),
+        "bw_paths is empty"
+    )
+    expect_error(
+        get_coverage(
+            bw_paths = "~/.cache/BiocFileCache/25c6571687b_SRR660824_SRS389722_SRX222703_male_lung.png",
+            auc_raw = gtex_metadata[["auc"]][1],
+            auc_target = 40e6 * 100,
+            chrs = c("chr21", "chr22")
+        ),
+        "Please check your bigwig file paths are correct"
+    )
+    expect_type(test_coverage, "list")
+    expect_type(test_coverage[["chr21"]][["meanCoverage"]], "S4") # output should be an Rle which is an S4
+    expect_equal(length(test_chrs), length(test_coverage))
 })
