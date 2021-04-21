671306194f23c3a7325c82e1a615460ea2db8c72
eolagbaju <eoolagbaju@gmail.com>
2021-04-21 13:54:54 +0000

    Format

--- R/data.R
+++ R/data.R
@@ -1,30 +1,30 @@
-#' An example auc value 
+#' An example auc value
 #'
 #' An auc value for a user to try out the package and to pass in for tests
 #'
 #' @format A numeric value
-#' 
+#'
 #' @source See example.R in data-raw
 #'
 "auc_example"
 #' An example coverage
-#' 
+#'
 #' An example coverage generated for a user to try out the package and
 #' to pass in for tests
-#' 
+#'
 #' @format A list of length 2 containing 2 Rles for chromosomes 21 and 22 respectively
-#' 
+#'
 #' @source See example.R in data-raw
-#' 
-"coverage_example" 
+#'
+"coverage_example"
 #' An example set of Expressed Regions
-#' 
+#'
 #' An example set of Expressed Regions generated for a user to try out the package and
 #' to pass in for tests
-#' 
-#' @format A list containing two lists (for each mcc) each with a set of genomic ranges 
+#'
+#' @format A list containing two lists (for each mcc) each with a set of genomic ranges
 #' for the different combinations of mcc and mrg
-#' 
+#'
 #' @source See example.R in data-raw
-#' 
+#'
 "ers_example"
