51a1637418052e112ec27800dc2a30c702978a41
eolagbaju <eoolagbaju@gmail.com>
2021-04-22 16:04:32 +0000

    Fix BiocCheck -https://github.com/eolagbaju/ODER/runs/2410722950

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+# Solve https://github.com/eolagbaju/ODER/runs/2410722950
+# https://github.com/LieberInstitute/megadepth/blob/master/R/globals.R
+# https://www.r-bloggers.com/2019/08/no-visible-binding-for-global-variable/
+utils::globalVariables(c(
+    "chrom"
+))
