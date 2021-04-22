f52be37d2147f1f39b34a5503073aedffec26cf5
eolagbaju <eoolagbaju@gmail.com>
2021-04-22 16:07:22 +0000

    Change tests and reduce line width

--- tests/testthat/test-get_coverage.R
+++ tests/testthat/test-get_coverage.R
@@ -13,7 +13,13 @@ test_that("get_chr_info works", {
     test_info <- get_chr_info(chrs = chrs, genome = "hg38")
 
     expect_false(test_info %contain% "chr4")
-    expect_error(get_chr_info(chrs = "chromononsense", genome = "hg38"), "Non-UCSC chromosome format was entered")
+    expect_error(
+        get_chr_info(
+            chrs = "chromononsense",
+            genome = "hg38"
+        ),
+        "Non-UCSC chromosome format was entered"
+    )
     expect_equal(nrow(test_info), length(chrs))
     expect_type(test_info[["chrom"]], "character")
     expect_type(test_info[[2]], "integer")
@@ -59,7 +65,10 @@ test_that("get_coverage works", {
     )
     expect_error(
         get_coverage(
-            bw_paths = "~/.cache/BiocFileCache/25c6571687b_SRR660824_SRS389722_SRX222703_male_lung.png",
+            bw_paths = paste0(
+                "~/.cache/BiocFileCache/25c6571687b_",
+                "SRR660824_SRS389722_SRX222703_male_lung.png"
+            ),
             auc_raw = auc_example,
             auc_target = 40e6 * 100,
             chrs = c("chr21", "chr22")
@@ -67,17 +76,35 @@ test_that("get_coverage works", {
         "Please check your bigwig file paths are correct"
     )
     expect_type(coverage_example, "list")
-    expect_type(coverage_example[["chr21"]][["meanCoverage"]], "S4") # output should be an Rle which is an S4
-    methods::is(coverage_example[["chr21"]][["meanCoverage"]], "Rle")
+    expect_type(coverage_example[["chr21"]][["meanCoverage"]], "S4")
+    expect_true(methods::is(coverage_example[["chr21"]][["meanCoverage"]], "Rle"))
     expect_equal(length(c("chr21", "chr22")), length(coverage_example))
 })
 
 test_that("get_ers works", {
-    expect_error(get_ers(mccs = c(5, 10), mrgs = c(10, 20)), "Coverage is missing")
-    expect_error(get_ers(coverage = coverage_example, mrgs = c(10, 20)), "Mean Coverage Cutoff is empty")
-    expect_error(get_ers(coverage = coverage_example, mccs = c(5, 10)), "Max Region Gap is empty")
+    expect_error(
+        get_ers(
+            mccs = c(5, 10),
+            mrgs = c(10, 20)
+        ),
+        "Coverage is missing"
+    )
+    expect_error(
+        get_ers(
+            coverage = coverage_example,
+            mrgs = c(10, 20)
+        ),
+        "Mean Coverage Cutoff is empty"
+    )
+    expect_error(
+        get_ers(
+            coverage = coverage_example,
+            mccs = c(5, 10)
+        ),
+        "Max Region Gap is empty"
+    )
 
     expect_type(ers_example, "list")
     expect_type(ers_example[[1]][[1]], "S4")
-    methods::is(ers_example[[1]][[1]], "GenomicRanges")
+    expect_true(methods::is(ers_example[[1]][[1]], "GenomicRanges"))
 })
--- tests/testthat/test-optimise_ERs.R
+++ tests/testthat/test-optimise_ERs.R
@@ -1,14 +1,32 @@
-gtf_url <- "http://ftp.ensembl.org/pub/release-103/gtf/homo_sapiens/Homo_sapiens.GRCh38.103.chr.gtf.gz"
-test_gtf_tempfile <- file.path(tempdir(), "gtf_file.gtx")
-download.file(url = gtf_url, destfile = test_gtf_tempfile)
+gtf_url <- paste0(
+    "http://ftp.ensembl.org/pub/release-103/",
+    "gtf/homo_sapiens/Homo_sapiens.GRCh38.103.chr.gtf.gz"
+)
+
+gtf_path <- ODER:::.file_cache(gtf_url)
+
+test_exons <- get_exons(
+    gtf = gtf_path,
+    ucsc_chr = TRUE,
+    ignore.strand = TRUE
+)
 
 test_that("get_exons works", {
     expect_error(
-        get_exons(gtf = test_gtf_tempfile, ucsc_chr = TRUE, ignore.strand = TRUE),
+        get_exons(
+            gtf = "gtf_file.gtx",
+            ucsc_chr = TRUE,
+            ignore.strand = TRUE
+        ),
         "Please check your gtf file path"
     )
-    # expect_type(coverage_example, "list")
-    # expect_type(coverage_example[["chr21"]][["meanCoverage"]], "S4") # output should be an Rle which is an S4
-    # methods::is(coverage_example[["chr21"]][["meanCoverage"]], "Rle")
-    # expect_equal(length(c("chr21", "chr22")), length(coverage_example))
+    expect_error(
+        get_exons(
+            gtf = "gtf_file.gtx.gz",
+            ucsc_chr = TRUE,
+            ignore.strand = TRUE
+        ),
+        "Please check your gtf file path"
+    )
+    expect_true(methods::is(test_exons, "GenomicRanges"))
 })
