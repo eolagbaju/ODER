5df8c7d9b5f4c2f2277a8eeb2b9a66cceae9ddeb
eolagbaju <eoolagbaju@gmail.com>
2021-04-23 15:58:22 +0000

    Fix https://github.com/eolagbaju/ODER/runs/2421079313

--- R/globals.R
+++ R/globals.R
@@ -2,5 +2,19 @@
 # https://github.com/LieberInstitute/megadepth/blob/master/R/globals.R
 # https://www.r-bloggers.com/2019/08/no-visible-binding-for-global-variable/
 utils::globalVariables(c(
-    "chrom"
+    "chrom",
+    "queryHits",
+    "subjectHits",
+    "n_dis_exons",
+    "seqnames",
+    "start",
+    "end",
+    "start1",
+    "end1",
+    "start_diff",
+    "end_diff",
+    "median",
+    "mcc",
+    "mrg",
+    "."
 ))
