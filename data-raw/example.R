173bfb80243ab23d17ad7169d3e45247d6123b4b
eolagbaju <eoolagbaju@gmail.com>
2021-05-06 12:34:57 +0000

    Add ers_delta_example data

--- data-raw/example.R
+++ data-raw/example.R
@@ -24,6 +24,13 @@ coverage_example <- ODER::get_coverage(
 
 ers_example <- ODER::get_ers(coverage = coverage_example, mccs = c(5, 10), mrgs = c(10, 20))
 
+gtf_url <- "http://ftp.ensembl.org/pub/release-103/gtf/homo_sapiens/Homo_sapiens.GRCh38.103.chr.gtf.gz"
+gtf_path <- ODER:::.file_cache(gtf_url)
+exons_no_overlap <- get_exons(gtf = gtf_path, ucsc_chr = TRUE, ignore.strand = TRUE)
+
+ers_delta_example <- get_ers_delta(ers = ers_example, opt_exons = exons_no_overlap, delta_fun = .delta)
+
 usethis::use_data(auc_example, overwrite = TRUE)
 usethis::use_data(coverage_example, overwrite = TRUE) # might have to exclude - 1.4MB
 usethis::use_data(ers_example, overwrite = TRUE)
+usethis::use_data(ers_delta_example, overwrite = TRUE)
