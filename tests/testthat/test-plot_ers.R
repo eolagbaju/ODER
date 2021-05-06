7589a2e11ba58050a264d5c70a8aeff4288c8c45
eolagbaju <eoolagbaju@gmail.com>
2021-05-06 13:41:37 +0000

    Add tests for plot_ers

---
+++ tests/testthat/test-plot_ers.R
@@ -0,0 +1,21 @@
+test_plots <- plot_ers(
+    ers_delta = ers_delta_example,
+    opt_mcc_mrg = c("mcc_10", "mrg_20")
+)
+
+test_that("plot_ers works", {
+    expect_error(
+        plot_ers(
+            ers_delta = ers_delta_example
+        ),
+        "The optimum MCC and/or MRG were not entered"
+    )
+    expect_error(
+        plot_ers(
+            opt_mcc_mrg = c("mcc_10", "mrg_20")
+        ),
+        "No ER deltas were entered"
+    )
+
+    expect_true(methods::is(test_plots, "ggarrange"))
+})
