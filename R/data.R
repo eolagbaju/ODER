f9796c9baefff86b0f02c0990056c8a0474bf7e7
eolagbaju <eoolagbaju@gmail.com>
2021-05-07 16:04:51 +0000

    Add description for the ers_delta_example

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' An example auc value
+#' An example AUC value
 #'
-#' An auc value for a user to try out the package and to pass in for tests
+#' An AUC(Area Under Coverage) value for a user to try out the package and to pass in for tests
 #'
 #' @format A numeric value
 #'
@@ -28,3 +28,14 @@
 #' @source See example.R in data-raw
 #'
 "ers_example"
+#' An example set of ER deltas
+#'
+#' This set of deltas was calculated using Expressed Regions from chromosomes 21
+#' and 22 and exons from the ensemble hg38 set
+#'
+#' @format A tibble/dataframe with the sums, means, medians, n_eq_0 and
+#' propor_eq_0 for each combination of mccs (5 & 10) and mrgs (10 & 20)
+#'
+#' @source See example.R in data-raw
+#'
+"ers_delta_example"
