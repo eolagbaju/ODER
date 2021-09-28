dbd445fa7cd46c5283c09ae85483a830dcedfadb
eolagbaju <eoolagbaju@gmail.com>
2021-09-28 13:50:18 +0000

    Add data.table utility function

---
+++ R/utils-dplyr-col.R
@@ -0,0 +1,12 @@
+#' dplyr column name argument entry
+#'
+#' @name :=
+#' @rdname data.table:::=
+#' @keywords internal
+#' @importFrom data.table :=
+#' @usage char :=
+#' @param char A variable containing a character string to act as the column
+#' name
+#' @return column name
+#' @noRd
+NULL
